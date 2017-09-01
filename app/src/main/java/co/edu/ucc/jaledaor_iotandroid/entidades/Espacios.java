b192be7d628baabdf3eae077b8bc8c46469545f0
jaledaor <hueckham@gmail.com>
2017-09-01 17:18:41 -0500

    version 2

---
+++ app/src/main/java/co/edu/ucc/jaledaor_iotandroid/entidades/Espacios.java
@@ -0,0 +1,48 @@
+package co.edu.ucc.jaledaor_iotandroid.entidades;
+
+/**
+ * Created by ADMIN on 01/09/2017.
+ */
+
+public class Espacios {
+    private int estado_sala;
+    private int estado_habitacion;
+    private int estado_cocina;
+    private int estado_bano;
+
+    public Espacios() {
+    }
+
+    public int getEstado_sala() {
+        return estado_sala;
+    }
+
+    public void setEstado_sala(int estado_sala) {
+        this.estado_sala = estado_sala;
+    }
+
+    public int getEstado_habitacion() {
+        return estado_habitacion;
+    }
+
+    public void setEstado_habitacion(int estado_habitacion) {
+        this.estado_habitacion = estado_habitacion;
+    }
+
+    public int getEstado_cocina() {
+        return estado_cocina;
+    }
+
+    public void setEstado_cocina(int estado_cocina) {
+        this.estado_cocina = estado_cocina;
+    }
+
+    public int getEstado_bano() {
+        return estado_bano;
+    }
+
+    public void setEstado_bano(int estado_bano) {
+        this.estado_bano = estado_bano;
+    }
+}
+
